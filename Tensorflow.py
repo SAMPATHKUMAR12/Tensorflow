9699f1bf3d2976b985c211fc6666804e285c3ee1
SAMPATHKUMAR12 <sditagi@gmail.com>
2024-10-24 22:19:28 +0200

    test

--- Tensorflow.py
+++ Tensorflow.py
@@ -3,6 +3,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 
 x = tf.constant(3, shape=(2,2))
-print(x) 
+print(x)
 
-# print(tf.zeros(3))
+y = tf.constant([[34,6,7],[6,1,9]])
+print(y)
+
+z = tf.ones((2,3))
+print(z)
+
